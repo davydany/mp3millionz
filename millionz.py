c3c67c3c37d49ffb50aa732e2809f35a47ebd315
David Daniel <davydany@gmail.com>
2022-11-21 09:38:06 -0600

    Updated project for 2022.

--- millionz.py
+++ millionz.py
@@ -3,7 +3,6 @@ import errno
 import os
 import requests
 import platform
-import urllib2
 import zipfile
 
 from selenium import webdriver
@@ -41,6 +40,7 @@ def mkdir_p(path):
         else: raise
 
 def download_file(url, output_dir):
+
     local_path = os.path.join(output_dir, url.split("/")[-1])
 
     # with open(local_path, 'wb') as handle:
@@ -56,11 +56,24 @@ def download_file(url, output_dir):
     #         handle.write(block)
     # return local_path
 
-    f = urllib2.urlopen(url)
-    data = f.read()
     with open(local_path, "wb") as handler:
-        handler.write(data)
+
+        response = requests.get(url, stream=True)
+        if not response.ok:
+            raise IOError(f"Unable to download the file { url }")
+
+        for block in response.iter_content(1024):
+            if not block:
+                break
+
+            handler.write(block)
     return local_path
+
+    # f = urllib2.urlopen(url)
+    # data = f.read()
+    # with open(local_path, "wb") as handler:
+    #     handler.write(data)
+    # return local_path
 
 
 def run(username, password, output_dir):
@@ -68,25 +81,33 @@ def run(username, password, output_dir):
     # determine chrome driver path
     chromedrivers_filenames = {
         '32bit' : {
-            'Linux': 'chromedriver-linux32',
-            'Darwin': 'chromedriver_mac32',
-            'Windows': 'chromedriver_win32.exe'
+            'Linux': 'NOT SUPPORTED',
+            'macOS': 'NOT SUPPORTED',
+            'Windows': 'NOT SUPPORTED'
         },
         '64bit' : {
             'Linux': 'chromedriver_linux64',
-            'Darwin': 'chromedriver_mac32',
-            'Windows': 'chromedriver_win32.exe'
+            'macOS': 'chromedriver_mac_arm64',
+            'Windows': 'chromedriver_win64.exe'
         }
     }
     basedir =  os.path.dirname(os.path.realpath(__file__))
     local_os = platform.platform().split("-")[0]
     local_arch = platform.architecture()[0]
     try:
+        print(f"Local Arch: { local_arch }")
+        print(f"Local OS:   { local_os }")
         driver_path = os.path.join(basedir, 'chromedriver', chromedrivers_filenames[local_arch][local_os])
-        print "Driver Path: ", driver_path
+
+        if 'NOT SUPPORTED' in driver_path:
+            print(f"The driver is not supported in your arch ({ local_arch }) or os ({ local_os })")
+            return
+
+        print("Driver Path: ", driver_path)
         browser = webdriver.Chrome(driver_path)
-    except Exception, e:
-        print "Unable to get Chrome Driver"
+    except Exception as e:
+        print("Unable to get Chrome Driver")
+        print(e)
         return
 
 
@@ -122,72 +143,72 @@ def run(username, password, output_dir):
     purchased_items = []
 
     # aggregate the download links
-    print "Aggregating..."
+    print("Aggregating...")
     for i, card in enumerate(download_cards):
         
         year, title, artist, album = None, None, None, None
         try:
-	        # retrieve information for the card
-	        title = card.find_element_by_tag_name("h3").text.strip()
-	        artist = title.split('\\')[0].replace("/", "-").strip()
-	        album = title.split('\\')[1].replace("/", "-").strip()
-	        year = card.find_element_by_css_selector(".fast-info .frst").text[6:].strip()
-	        download_link = card.find_element_by_css_selector(".zip-cd").get_attribute("href").strip()
-	        
+            # retrieve information for the card
+            title = card.find_element_by_tag_name("h3").text.strip()
+            artist = title.split('\\')[0].replace("/", "-").strip()
+            album = title.split('\\')[1].replace("/", "-").strip()
+            year = card.find_element_by_css_selector(".fast-info .frst").text[6:].strip()
+            download_link = card.find_element_by_css_selector(".zip-cd").get_attribute("href").strip()
+            
 
-	        # notify user
-	        item = {
-	            'current': i,
-	            'total_cards': total_cards,
-	            'artist': artist,
-	            'album': album,
-	            'year': year,
-	            'download_link': download_link
-	        }
-	        print "\t -- (%(current)s / %(total_cards)s) %(artist)s - %(year)s - %(album)s" % item
-        	purchased_items.append(item)
-        except Exception, e:
-        	
-	        item = {
-	            'current': i,
-	            'total_cards': total_cards,
-	            'artist': artist,
-	            'album': album,
-	            'year': year
-	        }
-	        print "\t -- (%(current)s / %(total_cards)s) %(artist)s - %(year)s - %(album)s" % item
-        	print "\t - Unable to aggregate %s" % e.message
+            # notify user
+            item = {
+                'current': i,
+                'total_cards': total_cards,
+                'artist': artist,
+                'album': album,
+                'year': year,
+                'download_link': download_link
+            }
+            print("\t -- (%(current)s / %(total_cards)s) %(artist)s - %(year)s - %(album)s" % item)
+            purchased_items.append(item)
+        except Exception as e:
+            
+            item = {
+                'current': i,
+                'total_cards': total_cards,
+                'artist': artist,
+                'album': album,
+                'year': year
+            }
+            print("\t -- (%(current)s / %(total_cards)s) %(artist)s - %(year)s - %(album)s" % item)
+            print("\t - Unable to aggregate %s" % e.message)
 
 
     # close browser
     browser.close()
 
     # start downloading
-    print "Downloading..."
+    print("Downloading...")
     for item in purchased_items:
 
         year = item['year']
         album = item['album']
         artist = item['artist']
         download_link = item['download_link']
-    	print "\t -- (%(current)s / %(total_cards)s) %(artist)s - %(year)s - %(album)s" % item
+        print("\t -- (%(current)s / %(total_cards)s) %(artist)s - %(year)s - %(album)s" % item)
         try:
 
             # determine local paths
             abs_local_path = os.path.join(output_dir, artist, "%s - %s" % (year, album))
             if os.path.exists(abs_local_path):
-                print "\t - Already Downloaded. Skipping..."
+                print("\t - Already Downloaded. Skipping...")
                 continue
             mkdir_p(abs_local_path)
-            print "\t - Downloading: %s" % download_link
+            print("\t - Downloading: %s" % download_link)
 
             downloaded_path = download_file(download_link, abs_local_path)
-            print "\t - Downloaded to: %s" % downloaded_path
+            print("\t - Downloaded to: %s" % downloaded_path)
             with zipfile.ZipFile(downloaded_path, 'r') as zf:
                 zf.extractall(os.path.dirname(downloaded_path))
             os.remove(downloaded_path)
-        except Exception, e:
-            print "\t - ERROR: Unable to download file: %s" % e.message
+        except Exception as e:
+            print("\t - ERROR: Unable to download file: %s" % e.message)
             raise
 
 
